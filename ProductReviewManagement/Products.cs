b04339ae1aeae45bb7a4a5fd0cb6a68c4ee475b3
aditya <vinchuaditya@gmail.com>
2022-03-31 16:06:23 +0530

    created product review management

---
+++ ProductReviewManagement/Products.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ProductReviewManagement
+{
+    internal class Products
+    {
+        public int productId { get; set; }
+        public int userId { get; set; }
+        public int rating { get; set; }
+        public string review { get; set; }
+        public bool feedback { get; set; }
+
+    }
+}
---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Products> products = new List<Products>()
+            {
+                new Products() { productId = 1, userId = 1, rating = 8, review = "good", feedback = true },
+                new Products() { productId = 2, userId = 2, rating = 7, review = "good", feedback = true },
+                new Products() { productId = 3, userId = 3, rating = 4, review = "bad", feedback = false },
+                new Products() { productId = 5, userId = 4, rating = 8, review = "good", feedback = true },
+                new Products() { productId = 5, userId = 5, rating = 7, review = "good", feedback = true },
+                new Products() { productId = 8, userId = 5, rating = 7, review = "good", feedback = true },
+                new Products() { productId = 8, userId = 4, rating = 5, review = "bad", feedback = false },
+                new Products() { productId = 8, userId = 3, rating = 6, review = "good", feedback = true },
+                new Products() { productId = 6, userId = 2, rating = 6, review = "good", feedback = true },
+                new Products() { productId = 7, userId = 1, rating = 4, review = "bad", feedback = false },
+        }
+    }
+}
