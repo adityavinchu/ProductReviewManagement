25acb2dd97ec7b36370d56d30c8997073a6377a3
aditya <vinchuaditya@gmail.com>
2022-03-31 16:39:48 +0530

    [Aditya]Added: UC3_return_records_where_rating_graterThan_3-ID_is_1_4_9

--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -8,6 +8,13 @@ namespace ProductReviewManagement
 {
     internal class Program
     {
+        public static void Display(List<Products> result)
+        {
+            foreach (var product in result)
+            {
+                Console.WriteLine("ProductID:" + product.productId + " UserID:" + product.userId + " Rating:" + product.rating);
+            }
+        }
         static void Main(string[] args)
         {
             List<Products> products = new List<Products>()
@@ -19,16 +26,22 @@ namespace ProductReviewManagement
                 new Products() { productId = 5, userId = 5, rating = 7, review = "good", isLike = true },
                 new Products() { productId = 8, userId = 5, rating = 7, review = "good", isLike = true },
                 new Products() { productId = 8, userId = 4, rating = 5, review = "bad", isLike = false },
-                new Products() { productId = 8, userId = 3, rating = 10, review = "good", isLike = true },
+                new Products() { productId = 4, userId = 3, rating = 10, review = "good", isLike = true },
                 new Products() { productId = 6, userId = 2, rating = 9, review = "good", isLike = true },
                 new Products() { productId = 7, userId = 1, rating = 4, review = "bad", isLike = false }
             };
+            //return record of top three ratings 
             var result = products.OrderByDescending(x => x.rating).Take(3);
             foreach (var product in result)
             {
                 Console.WriteLine(product.rating);
             }
-        Console.ReadLine();
+
+            //return all record from the list who’s rating are greater then 3 and ID=1 or 4 or 9
+            var IDArray = new int[] { 1, 4, 9 };
+            var results = products.Where(x => x.rating > 3 && IDArray.Contains(x.productId)).ToList();
+            Display(results);
+            Console.ReadLine();
         }
     }
 }
