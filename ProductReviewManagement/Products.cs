8216cbaf2b8bc12d4c3bc78f65c2e68c7211caab
aditya <vinchuaditya@gmail.com>
2022-03-31 16:10:09 +0530

    [Aditya]Added: UC1 Create Product review class with

--- ProductReviewManagement/Products.cs
+++ ProductReviewManagement/Products.cs
@@ -7,7 +7,7 @@ namespace ProductReviewManagement
         public int userId { get; set; }
         public int rating { get; set; }
         public string review { get; set; }
-        public bool feedback { get; set; }
+        public bool isLike { get; set; }
 
     }
 }
--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -12,16 +12,17 @@ namespace ProductReviewManagement
         {
             List<Products> products = new List<Products>()
             {
-                new Products() { productId = 1, userId = 1, rating = 8, review = "good", feedback = true },
-                new Products() { productId = 2, userId = 2, rating = 7, review = "good", feedback = true },
-                new Products() { productId = 3, userId = 3, rating = 4, review = "bad", feedback = false },
-                new Products() { productId = 5, userId = 4, rating = 8, review = "good", feedback = true },
-                new Products() { productId = 5, userId = 5, rating = 7, review = "good", feedback = true },
-                new Products() { productId = 8, userId = 5, rating = 7, review = "good", feedback = true },
-                new Products() { productId = 8, userId = 4, rating = 5, review = "bad", feedback = false },
-                new Products() { productId = 8, userId = 3, rating = 6, review = "good", feedback = true },
-                new Products() { productId = 6, userId = 2, rating = 6, review = "good", feedback = true },
-                new Products() { productId = 7, userId = 1, rating = 4, review = "bad", feedback = false },
+                new Products() { productId = 1, userId = 1, rating = 8, review = "good", isLike = true },
+                new Products() { productId = 2, userId = 2, rating = 7, review = "good", isLike = true },
+                new Products() { productId = 3, userId = 3, rating = 4, review = "bad", isLike = false },
+                new Products() { productId = 5, userId = 4, rating = 8, review = "good", isLike = true },
+                new Products() { productId = 5, userId = 5, rating = 7, review = "good", isLike = true },
+                new Products() { productId = 8, userId = 5, rating = 7, review = "good", isLike = true },
+                new Products() { productId = 8, userId = 4, rating = 5, review = "bad", isLike = false },
+                new Products() { productId = 8, userId = 3, rating = 6, review = "good", isLike = true },
+                new Products() { productId = 6, userId = 2, rating = 6, review = "good", isLike = true },
+                new Products() { productId = 7, userId = 1, rating = 4, review = "bad", isLike = false }
+            };
         }
     }
 }
